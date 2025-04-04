da0612ce1af31920d6fce34b17a1857cc3b19f0d
Bondaliname <lina@Lenovo-Thinkpad.>
2025-04-04 11:52:44 +0000

    Solution

--- task.sql
+++ task.sql
@@ -10,13 +10,13 @@ CREATE TABLE Countries (
 
 -- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
 
-CREATE TABLE GeoIPCache (
-    ID INT AUTO_INCREMENT,
-    IPRange VARCHAR(18),
+CREATE TABLE GeoIPCache(
+	ID INT,
+    IpRange VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY CountryID REFERENCES Countries(ID) ON DELETE SET NULL,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE SET NULl,
     PRIMARY KEY (ID)
-) ENGINE=Memory;
+) ENGINE=MEMORY;
 
 -- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
 
@@ -31,18 +31,17 @@ CREATE TABLE ProductDescription(
 
 -- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
 
-CREATE TABLE Logs (
-    ID INT AUTO_INCREMENT,
-    Timestamp TIMESTAMP,
-    Message VARCHAR(100),
-    PRIMARY KEY (ID)
-) ENGINE=Blackhole;
+CREATE TABLE Logs(
+	ID INT,
+    Time TIMESTAMP,
+    LogRecord VARCHAR(100),
+	PRIMARY KEY (ID)
+) ENGINE=BLACKHOLE;
 
 -- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 
-CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(50),
-    Orders INT,
-    PRIMARY KEY (Date, ProductName)
+CREATE TABLE ProductReporting(
+	Date DATE NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;
